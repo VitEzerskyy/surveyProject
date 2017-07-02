dbaf9b107452cddbb4a1bde062b1eb8faac8d192
VitEzerskyy <_ezerskyy@ukr.net>
2017-07-02 16:24:04 +0300

    local changes

--- src/AppBundle/EventListener/CustomExceptionListener.php
+++ src/AppBundle/EventListener/CustomExceptionListener.php
@@ -6,17 +6,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
 use Symfony\Component\HttpKernel\KernelInterface;
 
+/**
+ * Class CustomExceptionListener
+ * @package AppBundle\EventListener
+ */
 class CustomExceptionListener
 {
     private $templateEngine;
     private $kernel;
 
+    /**
+     * CustomExceptionListener constructor.
+     * @param EngineInterface $templateEngine
+     * @param KernelInterface $kernel
+     */
     public function __construct(EngineInterface $templateEngine, KernelInterface $kernel)
     {
         $this->templateEngine = $templateEngine;
         $this->kernel = $kernel;
     }
 
+    /**
+     * @param GetResponseForExceptionEvent $event
+     */
     public function onKernelException(GetResponseForExceptionEvent $event)
     {
         $env = $this->kernel->getEnvironment();
